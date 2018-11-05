7eb2f005549c95a0b21b6c3910dcc1b80e96d7c7
Sri Nath <sri.developer731@gmail.com>
2018-11-05 17:46:55 -0500

    Fix Synchronous Updation of Char Counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,11 +3,26 @@ const MAX_CHAR_LIMIT = 140;
 $(document).ready(() => {
   console.log('Document has loaded!');
 
-  $('textarea').keyup(function() {
+  $('.counter').text(function () {
+    let countAtReload = MAX_CHAR_LIMIT - $(this).siblings('textarea').val().length;
+
+    if (countAtReload < 0) {
+      $(this).removeClass('black-text').addClass('red-text');
+    } else {
+      $(this).removeClass('red-text').addClass('black-text');
+    }
+
+    return MAX_CHAR_LIMIT - $(this).siblings('textarea').val().length;
+  });
+
+
+  $('textarea').on('input', function() {
     let counter = MAX_CHAR_LIMIT - this.textLength;
-    $('.counter').text(counter);
+    $(this).siblings('.counter').text(counter);
     if (counter < 0) {
-      $('.counter').css('color', 'red');
+      $(this).siblings('.counter').removeClass('black-text').addClass('red-text');
+    } else {
+      $(this).siblings('.counter').removeClass('red-text').addClass('black-text');
     }
   });
 });
