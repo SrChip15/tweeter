5b644b9c008e5cc9b9eaaa74fa7a826ea39d6780
Sri Nath <sri.developer731@gmail.com>
2018-11-06 13:43:00 -0500

    Fetch Tweets with AJAX
    Remove all fake data from app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,71 +3,15 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// Fake data taken from tweets.json
-const data = [{
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts <script>alert('hello');</script> schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
 $(document).ready(function () {
+
+  function loadTweets() {
+    $.ajax('/tweets', {
+      method: "GET",
+    }).then(function (tweets) {
+      renderTweets(tweets);
+    })
+  }
 
   function createTweetElement(tweet) {
     const user = tweet.user;
@@ -96,21 +40,16 @@ $(document).ready(function () {
     return $tweet.append($header).append($body).append($footer);
   }
 
-  // Test / driver code (temporary)
-  let $tweet = createTweetElement(tweetData); // to see what it looks like
-   // $('.tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
   function renderTweets(tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
     for (const tweet of tweets) {
       $('.tweets-container').append(createTweetElement(tweet));
     }
   }
-  renderTweets(data);
 
-  $('form').submit(function(event) {
+  loadTweets();
+
+
+  $('form').submit(function (event) {
     event.preventDefault();
 
     let formData = $(this).serialize();
