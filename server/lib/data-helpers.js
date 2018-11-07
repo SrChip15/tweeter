ee6861e790056f072744a570b10982ef4d05d28b
Sri Nath <sri.developer731@gmail.com>
2018-11-07 15:07:46 -0500

    Remove Unused Code and Unnecessary Comments

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,28 +9,16 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function (newTweet, callback) {
-      // db.tweets.push(newTweet);
-
       db.collection("tweets")
       .insertOne(newTweet)
       .catch(console.log);
 
       db.close();
       callback(null, true);
-      // .then(() => {
-      //   console.log("Inserting record...");
-      //   return callback(null, true);
-      // })
-      // .catch(err => {
-      //   console.log(err);
-      // });
-
     },
 
-    // Get all tweets in `db`, sorted by newest first
+    // Get all tweets in `db`
     getTweets: function (callback) {
-      // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-      // callback(null, db.tweets.sort(sortNewestFirst));
       db.collection("tweets").find().toArray((err, tweets) => {
         if (err) {
           return callback(err);
