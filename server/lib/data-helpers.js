afaa29815d2cb5d3e28b6b76a75916e440cb409a
Sri Nath <sri.developer731@gmail.com>
2018-11-07 12:47:23 -0500

    Connect MongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -8,20 +8,35 @@ module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+    saveTweet: function (newTweet, callback) {
+      // db.tweets.push(newTweet);
+
+      db.collection("tweets")
+      .insertOne(newTweet)
+      .catch(console.log);
+
+      db.close();
+      callback(null, true);
+      // .then(() => {
+      //   console.log("Inserting record...");
+      //   return callback(null, true);
+      // })
+      // .catch(err => {
+      //   console.log(err);
+      // });
+
     },
 
     // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+    getTweets: function (callback) {
+      // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      // callback(null, db.tweets.sort(sortNewestFirst));
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, tweets);
       });
     }
-
   };
 }
